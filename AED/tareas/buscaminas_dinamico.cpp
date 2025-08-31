162a3a9fe2a4cba412e3ffeac54ef3ff0216b0d7
Edgard Rodriguez <edgard.rodriguez@ucsp.edu.pe>
2025-08-30 19:14:04 -0500

    Update buscaminas_dinamico.cpp

--- AED/tareas/buscaminas_dinamico.cpp
+++ AED/tareas/buscaminas_dinamico.cpp
@@ -2,10 +2,9 @@
 #include <random>  // mt19937, uniform_int_distribution
 using namespace std;
 
-
 void print(int** A, int fil ){
     for(int** i = A ; i < A+fil  ; i++){
-        for(  int* j = *i  ; j < *(i+1) ; j++){
+        for(  int* j = *i  ; j < *i+fil ; j++){
             if(*j==9){
                cout<<"* ";
             }
@@ -17,45 +16,31 @@ void print(int** A, int fil ){
     }
 } 
 
+void solucion(int** A, int fil ){                     //   100 101 102 103
+    for(int** i = A ; i < A+fil  ; i++){              //   200 201 202 203
+        for(  int* j = *i  ; j < *i+fil ; j++){       //   300 301 302 303 //   400 401 402 403
+            if( *j != 9 ){
+                if(  (i-1) >= A ){
+                    if( *( *(i-1) + (j-*i-1) ) == 9   &&   ( *(i-1) + (j-*i-1) )   >= *(i-1) ) (*j)++;     //   o 
+                    if( *( *(i-1) + (j-*i+0) ) == 9 )                                          (*j)++;       //      o 
+                    if( *( *(i-1) + (j-*i+1) ) == 9   &&   ( *(i-1) + (j-*i+1) )   <  *(i) )   (*j)++;      //         o 
+                }
+                if( *( j-1 ) ==9   && ( j-1 ) >= *i )      (*j)++;                        //   o
+                if( *( j+1 ) ==9   && ( j+1 ) <  *(i+1) )  (*j)++;                     //         o
 
-void solucion(int** A, int fil ){                //   100 101 102 103
-    for(int** i = A ; i < A+fil  ; i++){        //   200 201 202 203
-        for(  int* j = *i  ; j < *(i+1) ; j++){       //   300 301 302 303 //   400 401 402 403
-          if( *j != 9 ){
-            cout<< "--" <<endl;
-            cout<<  (j) <<endl;
-            cout<<  (*i) <<endl;
-            cout<<  (j-*i) <<endl;
-            cout<<  (j-*i-1) <<endl;
-            cout<< "--" <<endl;
-            if( *( *(i-1) + (j-*i-1) ) == 9 ){cout<<"s"<<endl; (*j)++;};    //   o 
-            if( *( *(i-1) + (j-*i+0) ) == 9 ) (*j)++;                       //      o 
-            if( *( *(i-1) + (j-*i+1) ) == 9 ) (*j)++;                       //         o 
-
-            if( *( j-1 ) ==9 ) (*j)++;                                      //   o
-            if( *( j+1 ) ==9 ) (*j)++;                                      //         o
-
-            if( *( *(i+1) + (j-*i-1) ) == 9 ) (*j)++;                       //   o 
-            if( *( *(i+1) + (j-*i+0) ) == 9 ) (*j)++;                       //      o 
-            if( *( *(i+1) + (j-*i+1) ) == 9 ) (*j)++;                       //         o 
-
-           //cout<< *j <<" ";*
+                if(  (i+1) < A+fil ){
+                    if( *( *(i+1) + (j-*i-1) ) == 9   &&   ( *(i+1) + (j-*i-1) )  >= *(i) ) (*j)++;       //   o 
+                    if( *( *(i+1) + (j-*i+0) ) == 9)                                        (*j)++;        //      o 
+                    if( *( *(i+1) + (j-*i+1) ) == 9   &&   ( *(i+1) + (j-*i+1))   < *(i+2)) (*j)++;       //         o 
+                }   
           }
         }
-        cout<<endl;
+        
     }
 }
 
-
-
-
-
-
 int main()
 {
-    cout<<"buscaminas\n";
-
-    // Configuración del generador aleatorio
     random_device rd;
     mt19937 gen(rd());
     uniform_int_distribution<> dist(0, 1); // devuelve 0 o 1  
@@ -79,26 +64,11 @@ int main()
             }
         }
     }
-    
-    /*int A[10][10]=   {{0,9,9,0,0,0,0,0,0,0},  //1
-                      {0,9,0,0,0,0,0,0,0,0},  //2
-                      {0,0,0,0,9,9,0,0,0,0},  //3
-                      {0,0,0,0,0,0,0,0,0,0},  //4
-                      {0,9,0,0,0,0,0,0,0,0},  //5
-                      {0,0,0,0,0,0,0,0,0,0},  //6
-                      {0,0,0,0,0,0,0,0,0,0},  //7
-                      {0,0,0,0,0,0,0,0,0,0},  //8
-                      {0,0,0,0,0,0,0,0,0,0},  //9
-                      {0,0,0,0,0,0,0,0,0,0},  //10
-                     };
-    */
 
-    // Dibujar matriz
     cout<<"solo minas"<<endl;
-    print(p,fil);           cout<<endl;cout<<endl;
-    solucion(p, 10 );       cout<<endl;cout<<endl;  
-    cout<<"solucion"<<endl;
+    print(p,fil);           
+    solucion(p, 10 );      
+    cout<<endl<<"solucion"<<endl;
     print(p,10);
-
     return 0;
 }
