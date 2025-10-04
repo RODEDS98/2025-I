10fafb6b9bee6631183f055739bf894f74325eb5
Edgard Rodriguez <edgard.rodriguez@ucsp.edu.pe>
2025-10-04 10:46:27 -0500

    Update cola2.cpp

--- AED/tareas/cola/cola2.cpp
+++ AED/tareas/cola/cola2.cpp
@@ -39,6 +39,7 @@ bool Cola<T>::lleno(){
 
 template<class T>
 bool Cola<T>::push(T a){
+    cout<<"agregando "<<endl;
     int dim= sizeof(A)/sizeof(*A);
 	if(vacio()){
 	    
@@ -86,7 +87,7 @@ bool Cola<T>::push(T a){
 
 template<class T>
 bool Cola<T>::pop(T &a){
-    cout<<"borrando"<<endl;
+    cout<<"borrando ";
     int dim= sizeof(A)/sizeof(*A);
 	if(vacio()){
 		cout<<"vacio"<<endl;
@@ -121,7 +122,7 @@ bool Cola<T>::pop(T &a){
 
 template<class T>
 void Cola<T>::print(){
-    int dim= sizeof(A)/sizeof(*A);cout<<"print"<<endl;
+    int dim= sizeof(A)/sizeof(*A);cout<<"print: "<<endl;
     //cout<<"Head"<<Head<<endl;cout<<"Tail"<<Tail<<endl;
     //cout<<sizeof(A)/sizeof(*A)<<endl;
     if(!vacio()){ //cout<<"Head"<<endl;
@@ -141,10 +142,9 @@ void Cola<T>::print(){
                 cout<<*i<<" ";
         
         }
-        cout<<endl;
     }
     else{
-        cout<<"vacio"<<endl;
+        cout<<" vacio "<<endl;
     }        
 }
 
@@ -153,7 +153,7 @@ int main(){
     Cola<int> Pint;
     //cout<<Pint.vacio()<<endl;
     //cout<<Pint.lleno()<<endl;
-    
+    int val;
     Pint.push(1);
     Pint.push(2);
     Pint.push(3);
@@ -164,38 +164,51 @@ int main(){
     Pint.push(8);
     Pint.push(9);
     Pint.push(10);
+    Pint.print();    cout <<endl;
+    
+    cout<<endl <<"_____INTENTANDO AGREGAR UN ELEMENTO MAS_____ "<<endl<<endl;
     Pint.push(11);
     //Pint.push(10);
-    Pint.print();
-    int val;
-    Pint.pop(val);
-    cout <<endl<<"val "<< val<<endl;
-    Pint.print();
-    Pint.push(100);Pint.print();
-    Pint.pop(val);Pint.print();
-    Pint.pop(val);Pint.print();
-    Pint.pop(val);Pint.print();
-    Pint.push(101);Pint.print();
-    Pint.push(102);Pint.print();
-    Pint.push(103);Pint.print();
-    Pint.push(100);Pint.print(); //lleno
+    Pint.print();    cout <<endl;
     
     
-    Pint.pop(val);Pint.pop(val);Pint.pop(val);Pint.pop(val);
-    Pint.pop(val);Pint.pop(val);Pint.pop(val); //borra 7 elementos
-    cout <<endl<<"val "<< val<<endl;
-    Pint.print();
-    Pint.pop(val);
-    Pint.print();
-    Pint.pop(val);
-    Pint.print();
-    Pint.pop(val);
-    Pint.print();
+    cout <<endl;
     
-    cout<<endl <<"p "<<endl;
-    Pint.push(100);Pint.print();
-    Pint.push(110);Pint.print();
-    Pint.push(120);Pint.print();
-    Pint.print();
+    cout<<endl <<"_____AVANZANDO HEAD Y TAIL PASA A LA IZQUIERDA_____ "<<endl<<endl;
+    Pint.pop(val);cout<<"val: "<< val<<endl;Pint.print();             cout <<endl;cout <<endl;
+    
+    Pint.push(100);Pint.print();                                            cout <<endl;cout <<endl;
+    
+    cout <<endl;
+    Pint.pop(val);cout <<"val: "<< val<<endl;Pint.print();                   cout <<endl;cout <<endl;
+    
+    Pint.pop(val);cout <<"val: "<< val<<endl;Pint.print();                   cout <<endl;cout <<endl;
+    Pint.pop(val);cout<<"val: "<< val<<endl;Pint.print();                    cout <<endl;cout <<endl;
+    Pint.push(101);Pint.print();                    cout <<endl;cout <<endl;
+    Pint.push(102);Pint.print();                    cout <<endl;cout <<endl;
+    Pint.push(103);Pint.print();                    cout <<endl;cout <<endl;
+    Pint.push(100);Pint.print();                    cout <<endl;cout <<endl; //lleno
+    
+    cout<<endl <<"_____BORRANDO TODOS LOS ELEMENTOS_____ "<<endl<<endl;
+    Pint.pop(val);cout <<endl;Pint.print();cout <<endl;
+    Pint.pop(val);cout <<endl;Pint.print();cout <<endl;
+    Pint.pop(val);cout <<endl;Pint.print();cout <<endl;
+    Pint.pop(val);cout <<endl;Pint.print();cout <<endl;
+    Pint.pop(val);cout <<endl;Pint.print();cout <<endl;
+    Pint.pop(val);cout <<endl;Pint.print();cout <<endl;
+    Pint.pop(val);cout <<endl;Pint.print(); cout <<endl;//borra 7 elementos
+
+    Pint.pop(val);cout <<endl;Pint.print(); cout <<endl;
+
+    Pint.pop(val);cout <<endl;Pint.print(); cout <<endl;
+    cout<<endl <<"_____BORRANDO ULTIMO ELEMENTO_____ "<<endl<<endl;
+    Pint.pop(val);cout <<endl;Pint.print(); cout <<endl;
+
+    
+    cout<<endl <<"_____DESDE CERO_____ "<<endl<<endl;
+    Pint.push(111);Pint.print();                            cout <<endl;
+    Pint.push(112);Pint.print();                            cout <<endl;cout <<endl;
+    Pint.push(123);Pint.print();                            cout <<endl;cout <<endl;
+
 	return 0;
 }
